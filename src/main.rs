4b29a9cc54571fa866c611a8da7a6e9e905e29f1
beppune <manzo.giuseppe@gmail.com>
2025-06-11 18:09:25 +0200

    not export

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,87 @@
-use std::collections::{HashMap, HashSet};
+use std::{collections::HashMap, fmt};
+
+fn display_vec<T: fmt::Display>(vec: &[T]) -> String {
+    let content = vec.iter()
+        .map(|item| format!("{}", item))
+        .collect::<Vec<_>>()
+        .join(" ");
+    format!("{{ {content} }}")
+}
 
 struct Nfa{
+    states: Vec<HashMap<char,Vec<u32>>>,
+    final_states: Vec<u32>,
+    start_state: u32
 }
 
 impl Nfa{
     fn new() -> Self {
-        Self {}
+        Self {
+            states: vec![],
+            final_states: vec![],
+            start_state: 0
+        }
     }
 
-    fn state(s:u32, i:char, to:&Vec<u32>) {
-        unimplemented!();
+    fn state(&mut self, s:usize, i:char, to:Vec<u32>) {
+
+        for _ in self.states.len()..=s {
+            self.states.push( HashMap::new() );
+        }
+
+        self.states[s] = HashMap::from( [(i, to)] )
+
     }
 
-    fn finals(s:&Vec<u32>) {
-        unimplemented!();
+    fn finals(&mut self, s:Vec<u32>) {
+        self.final_states = s;
     }
 
-    fn start(s:u32) {
-        unimplemented!();
+    fn start(&mut self, s:u32) {
+        self.start_state = s
+    }
+
+    fn not_export(&self) -> String {
+
+        let mut not = String::new();
+        not.push_str("digraph {\n\trankdir=\"LR\"\n");
+
+        for (i,m) in self.states.iter().enumerate() {
+            for (k, v) in m {
+                not.push_str( format!("\t{} -> {} [label=\"{}\"]\n", i, display_vec(v), k).as_str() );
+            }
+        }
+
+        for i in self.final_states.iter() {
+            not.push_str( format!("\n\t{i} [shape=\"doublecircle\"]").as_str() );
+        }
+
+        not.push_str( format!("\n\t_ -> {}\n\t_ [style=\"invis\", width=0]\n", self.start_state ).as_str() );
+        
+        not.push_str("\n}");
+        not
     }
 }
 
 
 fn main() {
-    let mut set = HashSet::new();
-
-    assert_eq!(set.insert(1), true);
-    assert_eq!(set.insert(1), false);
-
-    let mut state: HashMap<char, HashSet<u32>> = HashMap::new();
-
-    assert_eq!(state.insert('a', set), None);
 
     let mut nfa = Nfa::new();
 
     // a(a|b)*
     nfa.state(0, 'a', vec![1]);
-    nfa.state(1, '-', vec![2]);
-    nfa.state(2, '-', vec![3,9]);
-    nfa.state(3, '-', vec![4,6]);
+    nfa.state(1, 'e', vec![2]);
+    nfa.state(2, 'e', vec![3,9]);
+    nfa.state(3, 'e', vec![4,6]);
     nfa.state(4, 'a', vec![5]);
-    nfa.state(5, '-', vec![8]);
+    nfa.state(5, 'e', vec![8]);
     nfa.state(6, 'b', vec![7]);
     nfa.state(7, 'a', vec![8]);
-    nfa.state(8, '-', vec![3,9]);
+    nfa.state(8, 'e', vec![3,9]);
 
     nfa.finals(vec![9]);
     nfa.start(0);
+
+    println!("\n{}\n", nfa.not_export() );
 
 }
