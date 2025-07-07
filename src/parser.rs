82ca811c68df6a8d9cd36aa864bd83e9515f517c
beppune <manzo.giuseppe@gmail.com>
2025-07-07 15:24:25 +0200

    basic structure for parse_alt

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,9 @@
+//
+// regex   ::= alt
+// alt     ::= concat ('|' concat)*
+// concat  ::= repeat+
+// repeat  ::= atom ('*' | '+' | '?')?
+// atom    ::= literal | '.' | '(' regex ')'
 
 enum Ast {
     Empty,
@@ -7,5 +13,32 @@ enum Ast {
     Star(Box<Ast>),
 }
 
+type Result = std::result::Result<Ast, &'static str>;
 
-#[config(test)]
+fn parse_alt(input: &mut std::iter::Peekable<std::str::Chars<>>) -> Result {
+
+
+    let mut p = input.peek();
+    let ast = match p {
+        Some(c) => match c {
+            c if c.is_alphabetic() => {
+
+            }
+        },
+        _ =>  Ast::Empty
+    };
+
+    Ok(ast)
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_this() {
+        let input = "ab";
+        assert_eq!(true, true);
+    }
+}
