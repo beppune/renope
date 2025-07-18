c332b174cebfa8296b613468c3bfa90b23f7d298
beppune <manzo.giuseppe@gmail.com>
2025-07-18 12:59:13 +0200

    parse_alt

--- src/parser.rs
+++ src/parser.rs
@@ -36,6 +36,10 @@ type Input<'a> = std::iter::Peekable<std::str::Chars<'a>>;
 fn parse_repeat(input: &mut Input) -> Option<Ast> {
     let c:char = input.next()?;
 
+    if !c.is_alphabetic() {
+        return None;
+    }
+
     let ast:Ast;
 
     let repeat = input.peek();
@@ -47,7 +51,7 @@ fn parse_repeat(input: &mut Input) -> Option<Ast> {
                     let _ = input.next();
                     Ast::repeat(r, Literal(c))
                         .expect("PARSE_REPEAT: expected one of the repeat chars: *,+,?")
-                }
+                },
                 _ => Literal(c),
             };
         },
@@ -75,7 +79,27 @@ fn parse_concat(input: &mut Input) -> Option<Ast> {
 }
 
 fn parse_alt(input: &mut Input) -> Option<Ast> {
-    None
+
+    let mut repeat = vec![];
+
+    while let Some(&a) = input.peek() {
+        if a == '|' {
+            let _ = input.next();
+        } else {
+            if let Some(ast) = parse_concat(input) {
+                repeat.push( ast );
+            }
+        }
+    }
+
+    let mut it = repeat.into_iter();
+    let mut ast = it.next()?;
+    while let Some(tsa) = it.next() {
+        ast = Alt( Box::new(ast), Box::new(tsa) );
+    }
+
+    Some(ast)
+
 }
 
 #[cfg(test)]
@@ -83,6 +107,15 @@ mod test {
     use super::*;
 
     #[test]
+    fn test_alt() {
+        let s = "ac+s|b*|ef";
+        let ast = parse_alt(&mut s.chars().peekable());
+
+        dbg!(ast);
+    }
+
+    #[test]
+    #[ignore]
     fn tes_concat() {
         let s =  "ab+c*d?"; 
         println!("{s}");
