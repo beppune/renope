9e24207ed620ceaa54de0bf8d3f7691ba1b7eea3
beppune <manzo.giuseppe@gmail.com>
2025-07-21 16:12:17 +0200

    Display Trait

--- src/parser.rs
+++ src/parser.rs
@@ -16,6 +16,42 @@ enum Ast {
     Stop(&'static str),
 }
 
+struct AstPreOrderIter<'a> {
+    stack: Vec<&'a Ast>,
+}
+
+impl<'a> AstPreOrderIter<'a> {
+    fn new(ast:&'a Ast) -> Self {
+        Self {
+            stack: vec![ast],
+        }
+    }
+}
+
+impl<'a> Iterator for AstPreOrderIter<'a> {
+    type Item = &'a Ast;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let node:&'a Ast = self.stack.pop()?;
+        
+        match node {
+            Concat(ast, tsa) | Alt(ast, tsa) => {
+                self.stack.push( &**ast);
+                self.stack.push( &**tsa);
+            },
+
+            Star(ast) | Plus(ast) | Optional(ast) => {
+                self.stack.push( &**ast );
+            },
+
+            _ => {}
+        }
+
+        Some(node)
+
+    }
+}
+
 type AstRes = Result<Ast,&'static str>;
 
 impl Ast {
@@ -27,6 +63,23 @@ impl Ast {
             _ => Err("Expected one of repeat operators: *, +, ?"),
         }
     }
+
+    fn preorder_iter(& self) -> AstPreOrderIter<'_> {
+        AstPreOrderIter::new(self)
+    }
+}
+
+impl Display for Ast {
+   fn fmt(&self, f: &mut fmt::Formatter<'_'>) -> fmt::Result {
+       match &self {
+        Literal(c) | Stop(c) => write!(f, "{}", c),
+        Concat(ast, tsa) | Alt(ast,tsa) => write!(f, "{}{}", Display::fmt(ast,f), Display::fmt(tsa,f)),
+
+        Star(ast) => write!("{}*", Display::fmt(ast,f)),
+        Plus(ast) => write!("{}+", Display::fmt(ast,f)),
+        Optional(ast) => write!("{}?", Display::fmt(ast,f)),
+    }
+   }
 }
 
 use self::Ast::*;
@@ -44,67 +97,26 @@ fn parse_atom(input: &mut Input) -> Option<Ast> {
         return x.is_alphabetic();
     };
 
-    let c:char = input.next()?;
+    let c = input.peek()?;
 
     let ast:Ast;
 
-    if !is_atom(c) {
-        ast =  Stop("Expected atom");
+    if !is_atom(*c) {
+        ast = Stop( "Expected atom character" );
         return Some(ast);
     }
 
-    let p = input.peek();
-
-    if let Some(&r) = p {
-        if is_quantifier(r) {
-            ast = Ast::repeat(r, Literal(c)).unwrap();
-            return Some(ast);
-        }
-    }
-
-    ast = Literal(c);
-    Some(ast)
+    ast = Literal(*c);
+    let _ = input.next();
+    return Some(ast);
 }
 
 fn parse_concat(input: &mut Input) -> Option<Ast> {
-
-    let mut repeat = vec![];
-
-    while let Some(ast) = parse_atom(input) {
-        repeat.push( ast);
-    }
-
-    let mut it = repeat.into_iter();
-    let mut ast:Ast = it.next()?;
-    while let Some(tsa) = it.next() {
-        ast = Concat( Box::new(ast), Box::new(tsa) );
-    }
-
-    Some(ast)
+    None
 }
 
 fn parse_alt(input: &mut Input) -> Option<Ast> {
-
-    let mut repeat = vec![];
-
-    while let Some(&a) = input.peek() {
-        if a == '|' {
-            let _ = input.next();
-        } else {
-            if let Some(ast) = parse_concat(input) {
-                repeat.push( ast );
-            }
-        }
-    }
-
-    let mut it = repeat.into_iter();
-    let mut ast = it.next()?;
-    while let Some(tsa) = it.next() {
-        ast = Alt( Box::new(ast), Box::new(tsa) );
-    }
-
-    Some(ast)
-
+    None
 }
 
 #[cfg(test)]
@@ -112,14 +124,29 @@ mod test {
     use super::*;
 
     #[test]
+    fn test_it() {
+        let ast = Concat( Box::new( 
+                Concat( Box::new(Literal('8')), Box::new(Literal('r') ) )
+        ), Box::new( Literal('d' ) ) );
+
+        for a in ast.preorder_iter() {
+            dbg!(&a);
+        }
+
+    }
+
+    #[test]
+    #[ignore]
     fn test_atom() {
-        let mut it = "a?=".chars().peekable();
+        let mut it = "ab=c+".chars().peekable();
 
-        let mut ast = parse_atom(&mut it);
-        assert_eq!(Some(Ast::Optional(Box::new(Ast::Literal('a')))), ast); 
-
-        ast = parse_atom(&mut it);
-        assert_eq!(Some(Ast::Stop("Expected atom")), ast); 
+        while let Some(ast) = parse_atom(&mut it) {
+            if let Stop(msg) = ast {
+                println!("Error {}", msg);
+                break;
+            }
+            dbg!(ast);
+        }
 
     }
 
